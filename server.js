e2a68eda5eead9727545028afb059b853fbc639e
Rio Cantre <smittenaf301icu@gmail.com>
2021-07-12 14:34:37 +0200

    Added package.json, package-lock.json and modified server.js and website/style.css

--- server.js
+++ server.js
@@ -2,7 +2,11 @@
 projectData = {};
 
 // Require Express to run server and routes
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
+const app = express();
 // Start up an instance of app
 
 /* Middleware*/
@@ -11,9 +15,47 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
 
 // Setup Server
+const port = 8888;
+app.listen(port, () => {
+    console.log(`running on localhost: ${port}`)
+})
+
+// Create a GET route
+app.get('/get', (req, res) => {
+    res.send(projectData);
+    console.log(projectData);
+})
+
+app.get('/all', (req, res) => {
+    res.send(projectData);
+    console.log(projectData)
+})
+
+
+// Create a POST route
+app.post('/add', (req, res) => {
+    newEntry = {
+        location: req.body.location,
+        dt: req.body.dt,
+        temp: req.body.temp,
+        icon: req.body.icon,
+        description: req.body.description,
+        max: req.body.max,
+        min: req.body.min,
+        feelings: req.body.feelings
+    }
+
+    projectData = newEntry;
+    console.log(projectData);
+    res.send(projectData);
+
+})
+
+
